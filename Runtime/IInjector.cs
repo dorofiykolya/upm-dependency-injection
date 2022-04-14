446f12a04696b6e01947ad3139d698f835959984
dorofiykolya <dorofiykolya@gmail.com>
2022-04-14 22:15:59 +0300

    1.0.0

---
+++ Runtime/IInjector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Injections.Descriptions;
+
+namespace Injections
+{
+    public interface IInjector : IInject, IResolve, IEnumerable<KeyValuePair<Type, IResolver>>
+    {
+        void Register(Type type, IResolver resolver);
+        void UnRegister(Type type);
+        IResolver GetResolver(Type type, bool includeInParents);
+        DescriptionProvider DescriptionProvider { get; }
+        Type InjectAttribute { get; }
+    }
+}
